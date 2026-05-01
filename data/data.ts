4c628563987fd3b0ad744bd1bd2e24f341aa85d3
EvgenFilonov <evgeniyfilonov@gmail.com>
2026-05-01 21:59:41 +0300

    feat: add coming soon page with countdown timer and navigation components

--- data/data.ts
+++ data/data.ts
@@ -1,31 +1,31 @@
 const currentYear = new Date().getFullYear();
 const data = {
-    sitename: "Sandip Baikare Blog",
-    sitetagline: "Something great is on the way! ✨ 🔥",
-    siteurl: "https://github.com/baikaresandip",
+    sitename: "proecoglobal.com",
+    sitetagline: "Поки ви переглядаєте цю сторінку ми виробляємо добрива",
+    siteurl: "https://github.com/EFilonov",
     sitelogo: "",
-    title: "Coming Soon!",
-    description: "We&apos;re currently working 👨‍💻 hard to bring you something great, and we can&apos;t wait to share it with you 📅. Our team is putting the finishing touches on a new project 🚀 that we think you&apos;ll love 😍.",
-    newsletterheading: "Stay tuned for our live updates!",
-    copyrightText: `Copyright © ${currentYear} | Design and Developed By &nbsp;<a target="_blank" class="no-underline md:underline" href="https://github.com/baikaresandip">Baikare Sandip</a>`,
-    socialIconsHeading: "Follow Us 📣",
+    title: "Сайт на реконструкції!",
+    description: "Ми тяжко працюємо над створенням чогось нового та ефективного. Ми не можемо дочекатися, щоб поділитися з вами!",
+    newsletterheading: "Будь з нами на зв'язку!",
+    copyrightText: `Copyright © ${currentYear} | Design and Developed By &nbsp;<a target="_blank" class="no-underline md:underline" href="https://github.com/EFilonov">EFilonov</a>`,
+    socialIconsHeading: "Підписуйтесь на наші оновлення",
     hideSubscribeForm: false, // make true to disable subscription form 
     socialIcons: [
-        {
-            icon: "facebook",
-            link: "https://www.facebook.com/sandeep.baikare/",
-        },
-        {
-            icon: "twitter",
-            link: "https://twitter.com/BaikareSandeep",
-        },
-        {
-            icon: "linkedIn",
-            link: "https://www.linkedin.com/in/sandip-baikare/",
-        },
+        // {
+        //     icon: "facebook",
+        //     link: "https://www.facebook.com/sandeep.baikare/",
+        // },
+        // {
+        //     icon: "twitter",
+        //     link: "https://twitter.com/BaikareSandeep",
+        // },
+        // {
+        //     icon: "linkedIn",
+        //     link: "https://www.linkedin.com/in/sandip-baikare/",
+        // },
     ],
     hide :{
-        subscribeForm: false, // make true to disable subscription form         
+        subscribeForm: false , // make true to disable subscription form         
         header: false,
         content: false,
         footer: false,
